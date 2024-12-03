91a514482b1dead833dae2a730f538215d54ebbd
josefinkl <josefin.klangefjall@gmail.com>
2024-12-03 13:54:45 +0100

    Search for adress done, created for search with contains instead of equals

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -4,17 +4,18 @@ import java.util.List;
 public class PhoneBook {
     List<PersonClass> PhoneBookList = new ArrayList<PersonClass>();
     public PhoneBook() {
-        PersonClass person1 = new PersonClass("Adam", "Andersson", new ArrayList<>(),new Address(),20 );
+        PersonClass person1 = new PersonClass("Adam", "Andersson", new ArrayList<>(),new Address("Nyponvägen","21A","Karlstad",12345),20 );
         PhoneBookList.add(person1);
-        PersonClass person2 = new PersonClass("Eva", "Pettersson", new ArrayList<>(),new Address(),20 );
+        PersonClass person2 = new PersonClass("Eva", "Pettersson", new ArrayList<>(),new Address("Skogsvägen","25","Karlstad",12345),20 );
         PhoneBookList.add(person2);
         //System.out.println(PhoneBookList.toString());
     }
+    List<PersonClass> searchResult = new ArrayList<>();
 
     public List<PersonClass> SearchFirstName(String name){
-        List<PersonClass> searchResult = new ArrayList<>();
+        searchResult.clear();
         for (PersonClass person : PhoneBookList) {
-            if(person.getFirstName().equals(name)){
+            if(person.getFirstName().contains(name)){
                 searchResult.add(person);
             }
         }
@@ -23,9 +24,9 @@ public class PhoneBook {
     }
 
     public List<PersonClass> SearchLastName(String name){
-        List<PersonClass> searchResult = new ArrayList<>();
+        searchResult.clear();
         for (PersonClass person : PhoneBookList) {
-            if(person.getLastName().equals(name)){
+            if(person.getLastName().contains(name)){
                 searchResult.add(person);
             }
         }
@@ -33,5 +34,15 @@ public class PhoneBook {
         return searchResult;
     }
 
+    public List<PersonClass> SearchStreet (String name){
+        searchResult.clear();
+        for (PersonClass person : PhoneBookList) {
+            if(person.getAddress().getStreetName().contains(name)){
+                searchResult.add(person);
+            }
+        }
+        System.out.println(searchResult);
+        return searchResult;
+    }
 }
 
